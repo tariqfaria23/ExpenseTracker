f97b453c96dff4465f0a77f9d4732852fb60668e
faria <76790237+tariqfaria23@users.noreply.github.com>
2024-04-09 15:27:52 +0530

    updated login page

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,7 +1,8 @@
-import { auth, provider } from "../../config/firebase-config";
-import { signInWithPopup } from "firebase/auth";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
+
 import "./styles.css";
 
 export const Auth = () => {
@@ -20,17 +21,23 @@ export const Auth = () => {
     navigate("/expense-tracker");
   };
 
-  if (isAuth) {
-    return <Navigate to="/expense-tracker" />;
-  }
-
   return (
     <div className="login-page">
-      <p>Sign In With Google to Continue</p>
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        {" "}
-        Sign In With Google
-      </button>
+      <h1 className="heading">Expense Tracker</h1>
+      <p className="message">
+        Track your expenses effortlessly.
+      </p>
+      <div className="google-signin">
+        <button className="google-login-button" onClick={signInWithGoogle}>
+          Sign In With Google
+        </button>
+      </div>
+      {isAuth && <Navigate to="/expense-tracker" />}
+      <p className="lower-text">
+        <p>View this project on <a href = "https://github.com/tariqfaria23/ExpenseTracker" target="_blank"> <img className="github-logo" src = "src\assets\github-mark.png"></img>
+          </a>
+        </p>
+      </p>
     </div>
   );
 };
